5f227efd05fd135fd929ef48fea834be8cad7f32
Jenni <justlittleoldmejenn@gmail.com>
2022-12-09 17:52:43 -0800

    Did more cleanup

--- Assets/Scripts/ButtonToJenniSays.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class ButtonToJenniSays : MonoBehaviour {
-
-    public void ChangeToJenniSays() {
-        SceneManager.LoadScene("Jenni Says", LoadSceneMode.Single);
-    }
-}
